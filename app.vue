d371853b3fb083579c43c844370e2dcd540c547a
Ivana Sparic <sparicivana.biz@gmail.com>
2024-04-09 18:11:49 +0200

    Create responsive step indicator

--- app.vue
+++ app.vue
@@ -17,15 +17,15 @@
 @media screen and (min-width: 1024px) {
   main {
     display: grid;
-    place-items: center;
+    align-items: center;
   }
 
   .multi-step-form-container {
+    display: flex;
+    padding: var(--spacing-400);
+    border-radius: var(--border-radius-soft);
+    min-height: 70vh;
     background-color: var(--white);
-    padding: 1rem;
-    border-radius: 10px;
-    height: 70vh;
-    max-width: 940px;
   }
 
 }
--- components/TheStepIndicator.vue
+++ components/TheStepIndicator.vue
@@ -1,20 +1,32 @@
 <template>
-    <ol>
-        <li aria-current="step">
-            <span>step 1</span>
-            <span>your info</span>
+    <ol class="step__list">
+        <li
+            aria-current="step"
+            class="step__list-item active"
+        >
+            <p class="step__description">
+                <span class="step__counter">step 1</span>
+                <span>your info</span>
+            </p>
+
         </li>
-        <li>
-            <span>step 2</span>
-            <span>select plan</span>
+        <li class="step__list-item">
+            <p class="step__description">
+                <span class="step__counter">step 2</span>
+                <span>select plan</span>
+            </p>
         </li>
-        <li>
-            <span>step 3</span>
-            <span>add-ons</span>
+        <li class="step__list-item">
+            <p class="step__description">
+                <span class="step__counter">step 3</span>
+                <span>add-ons</span>
+            </p>
         </li>
-        <li>
-            <span>step 4</span>
-            <span>summary</span>
+        <li class="step__list-item">
+            <p class="step__description">
+                <span class="step__counter">step 4</span>
+                <span>summary</span>
+            </p>
         </li>
     </ol>
 </template>
@@ -26,4 +38,75 @@
 
 </script>
 
-<style scoped></style>
+<style scoped>
+.step__list {
+    display: flex;
+    gap: var(--spacing-400);
+    justify-content: center;
+    counter-reset: step-counter;
+    padding-block-start: var(--spacing-800);
+    height: 172px;
+    list-style: none;
+    color: var(--white);
+    font-weight: var(--font-weight-bolder);
+    text-transform: uppercase;
+
+    background-image: url(/images/bg-sidebar-mobile.svg);
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+.step__list-item::before {
+    display: grid;
+    place-items: center;
+    counter-increment: step-counter;
+    content: counter(step-counter);
+    background-color: transparent;
+    border: 2px solid var(--white);
+    border-radius: 50%;
+    height: 40px;
+    aspect-ratio: 1;
+}
+
+.active.step__list-item::before {
+    background-color: var(--pastel-blue);
+    border-color: var(--pastel-blue);
+    color: var(--marine-blue);
+}
+
+
+.step__description {
+    display: none;
+}
+
+@media screen and (min-width: 1024px) {
+
+    .step__list {
+        height: 568px;
+        flex-direction: column;
+        flex-basis: 30%;
+        align-items: flex-start;
+        justify-content: flex-start;
+        padding: var(--spacing-1000);
+
+        background-image: url(/images/bg-sidebar-desktop.svg);
+        background-size: auto;
+    }
+
+    .step__list-item {
+        display: flex;
+        align-items: center;
+        gap: var(--spacing-400);
+    }
+
+    .step__description {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .step__counter {
+        font-weight: var(--font-weight-normal);
+        color: var(--cool-gray);
+    }
+}
+</style>
